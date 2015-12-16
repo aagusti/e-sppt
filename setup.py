26dde0a40084eaddb78eab93cb7b02028238f800
Owo Sugiana <sugiana@gmail.com>
2015-12-16 13:03:29 +0700

    Penambahan paket di setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
-
+import sys
+import subprocess
 from setuptools import setup, find_packages
+
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.txt')) as f:
@@ -9,7 +11,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
-    'pyramid',
+    'pyramid >= 1.5, < 1.6a',
     'pyramid_chameleon',
     'pyramid_debugtoolbar',
     'pyramid_tm',
@@ -17,7 +19,22 @@ requires = [
     'transaction',
     'zope.sqlalchemy',
     'waitress',
+    'sqlalchemy-datatables == 0.1.6',
+    'psycopg2',
+    'pyJasper',
+    'colander',
+    'deform >= 2.0a2',
     ]
+
+if sys.argv[1:] and sys.argv[1] == 'develop-use-pip':
+    bin_ = os.path.split(sys.executable)[0]
+    pip = os.path.join(bin_, 'pip')
+    for package in requires:
+        cmd = [pip, 'install', package]
+        subprocess.call(cmd)
+    cmd = [sys.executable, sys.argv[0], 'develop']
+    subprocess.call(cmd)
+    sys.exit()
 
 setup(name='esppt',
       version='0.0',
